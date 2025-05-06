ffe2173a470cd169570fac58ed1743848547df74
voicedhealer <vivien.bernardot@gmail.com>
2025-05-06 17:35:12 +0200

    update files

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax" # Ligne 2 et 3 (la continuation compte)
-print(str[39:66], str[107:111], str[0:6]) # Ligne 4
+ language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[0:6]
+print(str)
