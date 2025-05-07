aa328ecfff5fa2b66a4b2e8b1c22b2e787c8cc0b
voicedhealer <vivien.bernardot@gmail.com>
2025-05-07 16:07:49 +0200

    update exercice positive or negative

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
+number = -1
 if number > 0:
     print("is positive")
-if number == 0:
+elif number == 0:
     print("is zero")
-if number < 0:
+elif number < 0:
     print("is negative")
     
