54227d154b7bb95f0b34b4efede5d6b7f8fe8afb
voicedhealer <vivien.bernardot@gmail.com>
2025-05-08 22:01:31 +0200

    add exercice alphabet

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -4,9 +4,10 @@ for alpha in range(97, 123):
     # On commence une boucle "for" qui parcourt
     # les nombres de 97 à 122 inclus (123 non inclus)
     # Dans la table ASCII, les codes de 97 à 122 correspondent
-    #  aux lettres minuscules de 'a' à 'z'    
+    # aux lettres minuscules de 'a' à 'z'
     print("{}".format(chr(alpha)), end="")
-    # Pour chaque nombre (alphas), on affiche le caractère correspondant grâce à chr()
+    # Pour chaque nombre (alphas), on affiche le caractère
+    # correspondant grâce à chr()
     # L'option end="" permet de ne pas aller à la ligne
-    #  après chaque caractère (tout est sur la même ligne)
+    # après chaque caractère (tout est sur la même ligne)
     
