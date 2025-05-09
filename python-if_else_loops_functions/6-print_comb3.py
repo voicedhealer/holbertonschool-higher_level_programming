e0a2023577e4f20161fd920484ff6e068256afa0
voicedhealer <vivien.bernardot@gmail.com>
2025-05-09 11:21:00 +0200

    add exercice print combi 3

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,9 +3,9 @@
 for i in range(0, 9):
     # Boucle sur les entiers de 0 à 8 inclus
     for j in range(i + 1, 10):
-    # Pour chaque i, boucle sur les entiers de i+1 à 9 inclus
-    # Cela garantit que j est toujours supérieur à i,
-    #  évitant les doublons et répétitions inverses
+        # Pour chaque i, boucle sur les entiers de i+1 à 9 inclus
+        # Cela garantit que j est toujours supérieur à i,
+        #  évitant les doublons et répétitions inverses
         print("{}{}".format(i, j), end=", " if i < 8 or j < 9 else "")
         # Affiche la paire de chiffres i et j collés (ex : "01", "12", "89")
         # Ajoute une virgule et un espace après chaque 
