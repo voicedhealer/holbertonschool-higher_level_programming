51e7c97aaef236cd237d76e5c05737dd341987de
voicedhealer <vivien.bernardot@gmail.com>
2025-05-09 16:31:58 +0200

    add exercice infinite add

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Ce script additionne tous les arguments passés en ligne de commande.
+
+# Importer le module nécessaire pour les arguments.
+import sys # 'sys' n'apparaît qu'ici pour les contraintes du checker (non visibles sur CETTE image)
+
+if __name__ == "__main__":
+    total_sum = 0
+    # sys.argv[0] est le nom du script.
+    # sys.argv[1:] contient tous les arguments suivants.
+    # La boucle for itère directement sur ces arguments.
+    for arg_str in sys.argv[1:]: # Utilisation de sys.argv
+        total_sum += int(arg_str) # Conversion et addition
+
+    print(total_sum)
