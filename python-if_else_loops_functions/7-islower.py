58bf32d689f1fcf25a160f882c1f5bcb417c716f
voicedhealer <vivien.bernardot@gmail.com>
2025-05-09 11:26:28 +0200

    add exercice islower

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def islower(c):
+    # Définit une fonction appelée islower
+    # qui prend un caractère 'c' en paramètre
+
+    if ord(c) >= 97 and ord(c) <= 122:
+        # ord(c) renvoie le code ASCII du caractère 'c'
+        # Les codes ASCII de 97 à 122 correspondent
+        # aux lettres minuscules ('a' à 'z')
+
+        return True
+        # Si 'c' est une lettre minuscule, 
+        # la fonction retourne True
+
+    return False
+    # Sinon, la fonction retourne False
