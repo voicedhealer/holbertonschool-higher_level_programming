1077eab24a2493907a470e25c02835dbade7dc69
voicedhealer <vivien.bernardot@gmail.com>
2025-05-09 14:06:13 +0200

    add exercice remove char

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 import random
 number = random.randint(-10000, 10000)
 last_digit = number % 10
--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,12 +1,16 @@
-def remove_char_at(str_input, n): # Respectez le prototype si c'est `str`
-    # Étape 1: Gérer les index n hors limites
-    if n < 0 or n >= len(str_input):
-        return str_input # Retourner la chaîne originale si n est invalide
+#!/usr/bin/python3
+"""
+Fonction qui crée une copie de la chaîne,
+en supprimant le caractère à la position n.
+"""
 
-    # Étape 2: Construire la nouvelle chaîne si n est valide
+def remove_char_at(str_input, n): # Si le proto impose 'str', utilisez 'str'
+    """Crée une copie de str_input sans le caractère à l'index n."""
+    if n < 0 or n >= len(str_input):
+        return str_input  # Retourne la chaîne originale si n est invalide
+
     new_string = ""
     for i in range(len(str_input)):
-        if i != n: # Si l'index actuel n'est pas celui à supprimer
-            new_string = new_string + str_input[i] # Ajouter le caractère
-
+        if i != n:  # Si l'index actuel n'est pas celui à supprimer
+            new_string = new_string + str_input[i]  # Ajouter le caractère
     return new_string
