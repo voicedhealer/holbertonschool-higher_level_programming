6c3f53fd1e0bf91c0ffb3ab8f071cb99f86148f8
voicedhealer <vivien.bernardot@gmail.com>
2025-05-09 11:10:05 +0200

    add exercice print combi

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -4,9 +4,9 @@ for i in range(100):
     # Boucle sur les entiers de 0 à 99 inclus (range(100) génère 0, 1, ..., 99)
     print("{:02d}".format(i) + (", " if i < 99 else "\n"), end="")
     # "{:02d}" : Affiche le nombre sur deux chiffres, avec un zéro devant
-    #  si nécessaire (ex : 01, 02, ..., 99)
+    # si nécessaire (ex : 01, 02, ..., 99)
     # + (", " if i < 99 else "\n") : Ajoute une virgule et
-    #  un espace après chaque nombre sauf le dernier,
-    #  où il ajoute un retour à la ligne
-    # end="" : Empêche print d'ajouter un retour à la ligne automatique, 
+    # un espace après chaque nombre sauf le dernier,
+    # où il ajoute un retour à la ligne
+    # end="" : Empêche print d'ajouter un retour à la ligne automatique,
     # pour tout afficher sur une seule ligne (sauf le dernier nombre)
