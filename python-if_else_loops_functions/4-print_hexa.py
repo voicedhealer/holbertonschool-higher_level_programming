df433ca401be7d193d7077798d93cad0d5816835
voicedhealer <vivien.bernardot@gmail.com>
2025-05-09 10:15:30 +0200

    add exercice digit and hexa

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -3,5 +3,6 @@
 for i in range(99):
 # Boucle sur les entiers de 0 à 98 inclus (range(99) génère 0, 1, ..., 98)
     print("{:d} = 0x{:x}".format(i, i))
-    # Affiche chaque entier 'i' en décimal (d) puis en hexadécimal (x) avec le préfixe '0x'
+    # Affiche chaque entier 'i' en décimal (d)
+    #  puis en hexadécimal (x) avec le préfixe '0x'
     # Exemple : "42 = 0x2a"
