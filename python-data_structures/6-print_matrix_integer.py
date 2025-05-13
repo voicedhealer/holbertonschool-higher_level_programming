80e042c6ea72e44f6f619e562703c9118f6d5161
voicedhealer <vivien.bernardot@gmail.com>
2025-05-13 11:23:02 +0200

    exercice enter in the matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    # Boucle externe pour chaque ligne de la matrice
     for row in matrix:
-        numbers_string = []        
-    for numbers in row:
-        numbers_format = "{:d}".format(numbers)
-        numbers_string.append(numbers_format)
-    line_print = " ".join(numbers_string)
-    print(line_print)
+        # Initialiser une liste pour stocker les versions 'chaîne' des nombres
+        # de la ligne actuelle. Ceci doit être fait pour CHAQUE nouvelle ligne.
+        numbers_string_for_this_row = []
+
+        # Boucle interne pour chaque nombre dans la ligne actuelle
+        # Cette boucle DOIT être indentée pour être à l'intérieur de la boucle "for row in matrix"
+        for number_element in row:
+            # Formater le nombre en chaîne
+            numbers_format = "{:d}".format(number_element)
+            # Ajouter le nombre formaté à notre liste temporaire
+            numbers_string_for_this_row.append(numbers_format)
+
+        # Joindre toutes les chaînes de nombres de la ligne avec un ESPACE
+        # Cette opération DOIT être indentée pour être à l'intérieur de la boucle "for row in matrix"
+        line_to_print = " ".join(numbers_string_for_this_row)
+
+        # Afficher la ligne construite
+        # Cette opération DOIT être indentée pour être à l'intérieur de la boucle "for row in matrix"
+        print(line_to_print)
