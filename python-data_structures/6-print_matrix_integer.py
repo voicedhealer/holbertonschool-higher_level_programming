0f1e81c6d85f3e622dce68c95d0350678c551f92
voicedhealer <vivien.bernardot@gmail.com>
2025-05-13 11:20:15 +0200

    exercice enter in the matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        numbers_string = []        
+    for numbers in row:
+        numbers_format = "{:d}".format(numbers)
+        numbers_string.append(numbers_format)
+    line_print = " ".join(numbers_string)
+    print(line_print)
