92889e5014a5d25204ea93850cecaf93bb773591
voicedhealer <vivien.bernardot@gmail.com>
2025-05-13 16:34:45 +0200

    exercice divisible

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    result_list = []
+    for element in my_list:
+        if element % 2 == 0:
+            result_list.append(True)
+        else:
+            result_list.append(False)
