5721fef85daa015915c390fbe16b421d6afae32f
voicedhealer <vivien.bernardot@gmail.com>
2025-05-13 11:07:55 +0200

    exercice no c

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def no_c(my_string):
+    nouvelle_chaine = ""
+    for caractere in my_string:
+        if caractere != 'c' and caractere != 'C':
+         nouvelle_chaine += caractere
+    return nouvelle_chaine
