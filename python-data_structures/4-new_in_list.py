60f3dce0b2b20ac9facf686c5213e2f567319bfe
voicedhealer <vivien.bernardot@gmail.com>
2025-05-13 10:31:19 +0200

    exercice copie new list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if my_list is None:
-            return None
-    
+        return None
+
     copied_list  = my_list[:]
 
     if idx < 0:
-            return copied_list
+        return copied_list
     if idx >= len(my_list):
         return copied_list
 
