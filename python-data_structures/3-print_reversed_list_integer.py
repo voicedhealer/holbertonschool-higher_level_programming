2a4fb474c957787a046c566e9af7c4a87e27d3aa
voicedhealer <vivien.bernardot@gmail.com>
2025-05-13 10:12:07 +0200

    exercice print reverse

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -7,4 +7,3 @@ def print_reversed_list_integer(my_list=[]):
     for i in range(len(my_list) - 1, -1, -1):
         # Affiche chaque élément sous forme d'entier
         print("{:d}".format(my_list[i]))
-    
