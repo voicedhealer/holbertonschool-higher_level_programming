e869913dd159db3254d2d123a7950eb7bc17cfa1
voicedhealer <vivien.bernardot@gmail.com>
2025-05-13 15:55:44 +0200

    exercice multiple returns

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def multiple_returns(sentence):
+    len_char = len(sentence)
+
+    if not sentence:
+        first_char = None
+    else:
+        first_char = sentence[0]
+        
+    return(len_char, first_char)
