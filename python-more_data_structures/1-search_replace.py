2d5fdd47179de23886aeaf017c7613e255e053ac
voicedhealer <vivien.bernardot@gmail.com>
2025-05-14 11:38:21 +0200

    add exercice search remplace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    # Crée une nouvelle liste en remplaçant les éléments correspondants
+    new_list = []
+    for item in my_list:
+        # Remplace si l'élément correspond à 'search'
+        if item == search:
+            new_list.append(replace)
+        else:
+            new_list.append(item)
+    # Retourne la nouvelle liste
+    return new_list
