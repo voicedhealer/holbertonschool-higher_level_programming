c3bddfa2c174f630a179646ef508c4c8c0e1cebb
voicedhealer <vivien.bernardot@gmail.com>
2025-05-14 15:42:33 +0200

    add exercice best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    # Gérer le cas du dictionnaire vide
     if not a_dictionary:
         return None
+
+    # Initialiser les variables
     best_key = None
     max_score = None
+
+    # Parcourir chaque paire (clé, valeur) dans le dictionnaire
     for key, score_value in a_dictionary.items():
+        # Comparer et mettre à jour si un meilleur score est trouvé
         if max_score is None or score_value > max_score:
             max_score = score_value
             best_key = key
-    return best_score
+
+    # Une fois la boucle terminée
+    # (tous les éléments ont été vérifiés),
+    # retourner la clé du meilleur score trouvé.
+    return best_key
