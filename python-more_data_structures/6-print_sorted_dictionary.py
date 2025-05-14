9d4562abab1149855dfae565c33955689555e972
voicedhealer <vivien.bernardot@gmail.com>
2025-05-14 14:13:20 +0200

    add exercice print sorted

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    cles_triees = sorted(a_dictionary)
+    for cle in cles_triees:
+        valeur = a_dictionary[cle]
+        print("{}: {}".format(cle, valeur))
