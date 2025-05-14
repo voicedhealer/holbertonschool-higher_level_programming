635f6c9dbe907218efc4daad19c635c21b05d124
voicedhealer <vivien.bernardot@gmail.com>
2025-05-14 15:39:18 +0200

    add exercice best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -7,5 +7,5 @@ def best_score(a_dictionary):
     for key, score_value in a_dictionary.items():
         if max_score is None or score_value > max_score:
             max_score = score_value
-            best_score = key
-            return best_score
+            best_key = key
+    return best_score
