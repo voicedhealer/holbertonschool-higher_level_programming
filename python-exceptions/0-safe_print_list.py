95e683df3b1311fddfcae0d10f24c7bffa69bbab
voicedhealer <vivien.bernardot@gmail.com>
2025-05-15 16:14:26 +0200

    exercice safe print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,19 +6,23 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             # Essayer d'accéder à l'élément et de l'afficher
-            element_to_print = my_list[i]  # Obtenir l'élément
-            print(element_to_print, end="") # L'afficher SANS retour à la ligne
-            elements_printed_count += 1    # Si l'affichage a réussi, incrémenter le compteur
+            element_to_print = my_list[i]
+            print(element_to_print, end="")
+            elements_printed_count += 1
         except IndexError:
-            # Si on essaie d'accéder à un indice qui n'existe pas dans my_list,
+            # Si on essaie d'accéder à un indice 
+            # qui n'existe pas dans my_list,
             # une IndexError est levée. On l'attrape ici.
-            # Cela signifie qu'on a atteint la fin de la liste (ou que x est trop grand).
+            # Cela signifie qu'on a atteint la fin de la liste
+            # (ou que x est trop grand).
             # On doit donc arrêter d'essayer d'afficher.
             break  # Sortir de la boucle for
 
-    # Après la boucle (soit parce qu'on a affiché 'x' éléments, soit parce qu'on a 'break' à cause d'une IndexError),
+    # Après la boucle (soit parce qu'on a affiché 'x'
+    # éléments, soit parce qu'on a 'break' à cause d'une IndexError),
     # afficher un retour à la ligne final.
     print() # Un print vide fait un retour à la ligne
 
-    # Retourner le nombre d'éléments qui ont été effectivement affichés.
+    # Retourner le nombre d'éléments qui 
+    # ont été effectivement affichés.
     return elements_printed_count
