b74e42b8e23e12e20a923e9727112e68925d23f8
voicedhealer <vivien.bernardot@gmail.com>
2025-05-15 16:33:26 +0200

    exercice safe print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print(my_list[i], end="")
-            elements_printed_count += 1 # Incrémente le compteur
+            elements_printed_count += 1
         except IndexError:
             break
     print()
-    return elements_printed_count # Retourne le compteur
+    return elements_printed_count
