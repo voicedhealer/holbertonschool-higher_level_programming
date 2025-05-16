b45257cdeec323b728d70c65661f5869cfc89302
voicedhealer <vivien.bernardot@gmail.com>
2025-05-16 11:06:08 +0200

    exercice safe print division

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    res = []
+    for i in range(list_length):
+        try:
+            res += [my_list_1[i] / my_list_2[i]]
+        except ZeroDivisionError:
+            res += [0]
+            print("division by 0")
+        except TypeError:
+            res += [0]
+            print("wrong type")
+        except IndexError:
+            res += [0]
+            print("out of range")
+        finally:
+            continue
+    return res
