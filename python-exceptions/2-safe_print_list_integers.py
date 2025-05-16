00cb957c9e6fed626fd863d72fc8ba8da7fd2581
voicedhealer <vivien.bernardot@gmail.com>
2025-05-16 10:48:20 +0200

    exercice safe print list integer

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    element_printed_count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            elements_printed_count += 1
+        except (ValueError, TypeError):
+            continue
+        print("")
+        return element_printed_count
