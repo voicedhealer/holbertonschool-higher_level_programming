bcc3d0226b6091b3afec3928af454c2439535196
voicedhealer <vivien.bernardot@gmail.com>
2025-05-16 10:28:07 +0200

    exercice safe print integer

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -6,7 +6,7 @@ def safe_print_integer(value):
         # Si les lignes ci-dessus s'exécutent sans erreur,
         # cela signifie que 'value' a été traitée comme un entier.
         return True
-    except (ValueError, TypeError): # Ou plus généralement "except Exception:"
+    except (ValueError, TypeError):  # Ou plus généralement "except Exception:"
         # Si une ValueError (ex: format("hello")) ou TypeError (ex: format([]))
         # se produit lors du formatage, on arrive ici.
         # (La question de l'affichage sur stderr reste si sys est interdit)
