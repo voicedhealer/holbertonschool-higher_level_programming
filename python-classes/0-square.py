3516b2124a6f876a48d466fed1ef04cc079bb48e
voicedhealer <vivien.bernardot@gmail.com>
2025-05-19 15:37:12 +0200

    exercice 0 square

---
+++ python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square :
+    """
+    Ceci est une classe vide.
+    Elle ne définit aucun attribut ni aucune méthode pour le moment.
+    Elle peut être utilisée comme point de départ ou comme placeholder.
+    """
+    pass
