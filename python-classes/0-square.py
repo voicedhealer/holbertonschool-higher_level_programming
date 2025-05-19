d9b1f780980f37a8b3d865c791c9038af2547ca0
voicedhealer <vivien.bernardot@gmail.com>
2025-05-19 16:18:07 +0200

    exercice 1 square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
 """
-Ce module définit une classe Square.
-La classe Square définit un carré par sa taille.
+Une classe vide qui définit un carré.
 """
 
 
 class Square:
     """
-    Représente un carré
-
-    Attributs privés:
-        __size (int): La taille du cotédu carré
+    Private instance attribute
     """
-    __size = None
-
-def __init__(self, size):
-    """
-    Initialise ube nouvelle intance de Square
-
-    Args:
-        size (int): La taille du coté du carré
-    """
-    self.__size = size
+    _size = None
+    pass
---
+++ python-classes/1-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Ce module définit une classe Square.
+La classe Square définit un carré par sa taille.
+"""
+
+
+class Square:
+    """
+    Représente un carré
+
+    Attributs privés:
+        __size (int): La taille du cotédu carré
+    """
+    __size = None
+
+def __init__(self, size):
+    """
+    Initialise ube nouvelle intance de Square
+
+    Args:
+        size (int): La taille du coté du carré
+    """
+    self.__size = size
