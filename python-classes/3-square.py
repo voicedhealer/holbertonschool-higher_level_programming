d45b5f8a870020d7b848c457e1cf4f98c5acdd63
voicedhealer <vivien.bernardot@gmail.com>
2025-05-20 10:24:31 +0200

    exercice 3 square

---
+++ python-classes/3-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Ce module définit une classe Square.
+La classe Square définit un carré par sa taille.
+"""
+
+
+class Square:
+    """
+    Représente un carré
+
+    Attributs privés:
+    __size (int): La taille du cotédu carré
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialise ube nouvelle intance de Square
+        Args:
+        size (int): La taille du coté du carré
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """
+         Calcule et retourne l'aire actuelle du carré.
+
+        Returns:
+            int: L'aire du carré (size * size).
+        """
+        return self.__size ** 2
