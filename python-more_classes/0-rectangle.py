0ddcc345998f4f5122d9bb4b7f21fca0e347866d
voicedhealer <vivien.bernardot@gmail.com>
2025-05-21 10:33:12 +0200

    exercice 0 rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+construction de la class rectangle
+"""
+
+
+class Rectangle:
+    """
+    initiallisation de size à None et private _
+    """
+
+    _size  = None
+
+    pass
