f31b892127225119990bf11d4489c468c6861af4
voicedhealer <vivien.bernardot@gmail.com>
2025-05-26 16:53:36 +0200

    exercice 2 my list

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+la fonction retourne Vrai si elle exactememnt une instance de classe
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Méthode is_same_class 
+    """
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
