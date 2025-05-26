d029eb31536a7f99ac176fd5babd82b71da9c515
voicedhealer <vivien.bernardot@gmail.com>
2025-05-26 17:00:08 +0200

    exercice 2 my list

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -8,7 +8,7 @@ def is_same_class(obj, a_class):
     """
     Méthode is_same_class
     """
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     else:
         return False
