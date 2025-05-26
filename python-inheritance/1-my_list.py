d6d101585cff48ef702508c26fe564deb1ca96f8
voicedhealer <vivien.bernardot@gmail.com>
2025-05-26 15:26:29 +0200

    exercice 1 my list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+class Mylist(list):
+    """
+    Classe Mylist qui hérite de la calsse list
+    """
+    def print_sorted(self):
+        """
+        Méthode publique print_stored qui affiche
+        les éléments de la liste
+        """
+        liste_triee_pour_impression = sorted(self)
+        print(liste_triee_pour_impression)
