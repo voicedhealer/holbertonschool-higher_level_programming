ac9184fba7a1c9667de84605172bff1c925caa82
voicedhealer <vivien.bernardot@gmail.com>
2025-05-27 11:42:21 +0200

    exercice 6 base geo

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+Ce module définit une classe de base pour la géométrie, BaseGeometry.
+"""
+
+
+class BaseGeometry:
+
+    """
+    Une classe de base pour représenter des formes géométriques.
+    Contient une méthode 'area' qui doit être implémentée par les sous-classes.
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
