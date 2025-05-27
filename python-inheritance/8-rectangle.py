575f1eb44a8494c774aff3d47127ec23df717e41
voicedhealer <vivien.bernardot@gmail.com>
2025-05-27 15:01:03 +0200

    exercice 8 rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python3
 """
-Ce module définit une classe Rectangle.
+Ce module définit une classe Rectangle qui hérite de BaseGeometry.
 """
 
 
-def __init__(self, width, height):
-    """
-    Initialise un rectangle avec une largeur et une hauteur.
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-    Args:
-        width (int): La largeur du rectangle.
-        height (int): La hauteur du rectangle.
 
-    Raises:
-        TypeError: Si 'width' ou 'height' n'est pas un entier.
-        ValueError: Si 'width' ou 'height' est inférieur ou égal à 0.
+class Rectangle(BaseGeometry):
     """
-    self.integer_validator("width", width)
-    self.integer_validator("height", height)
-    self.__width = width
-    self.__height = height
+    Représente un rectangle.
+
+    Hérite de BaseGeometry et utilise integer_validator pour
+    valider la largeur et la hauteur.
+    """
+    def __init__(self, width, height):
+        """
+        Initialise un nouveau Rectangle.
+
+        Args:
+            width (int): La largeur du rectangle. Doit être un entier positif.
+            height (int): La hauteur du rectangle. Doit être un entier positif.
+
+        Raises:
+            TypeError: Si width ou height n'est pas un entier.
+            ValueError: Si width ou height est <= 0.
+        """
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
