95ccf9b77d2696efda8e89cde84c9bfe2458300f
voicedhealer <vivien.bernardot@gmail.com>
2025-05-27 14:51:47 +0200

    exercice 8 rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+Ce module définit une classe Rectangle.
+"""
+
+
+def __init__(self, width, height):
+    """
+    Initialise un rectangle avec une largeur et une hauteur.
+
+    Args:
+        width (int): La largeur du rectangle.
+        height (int): La hauteur du rectangle.
+
+    Raises:
+        TypeError: Si 'width' ou 'height' n'est pas un entier.
+        ValueError: Si 'width' ou 'height' est inférieur ou égal à 0.
+    """
+    self.integer_validator("width", width)
+    self.integer_validator("height", height)
+    self.__width = width
+    self.__height = height
