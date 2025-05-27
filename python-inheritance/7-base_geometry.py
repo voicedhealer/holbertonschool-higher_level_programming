8d8d040426dcd3f3fc7f75e1ff2d38ddf19ba85b
voicedhealer <vivien.bernardot@gmail.com>
2025-05-27 14:36:12 +0200

    exercice 7 base geo

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -38,5 +38,5 @@ class BaseGeometry:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-        return value
+
 
