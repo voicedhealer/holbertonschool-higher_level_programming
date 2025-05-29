78ee7030af912a9ba29abbde1e75d64b345c43bd
voicedhealer <vivien.bernardot@gmail.com>
2025-05-29 15:37:29 +0200

    exercice 01 duck typing

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -4,6 +4,7 @@ Module illustrant les classes de base abstraites
 avec Shape, area, perimeter
 """
 from abc import ABC, abstractmethod
+import math
 
 
 class Shape(ABC):
@@ -30,11 +31,30 @@ class Circle(Shape):
     """
     Class cercle calcul de la surface d'un cercle avec pi
     """
-    pass
+    def __init__(self, radius):
+        if radius < 0:
+            raise ValueError("Radius cannot be negative")
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
 
 
 class Rectangle(Shape):
     """
     Class Rectangle
     """
-    pass
+    def __init__(self, width, height):
+        if width < 0 or height < 0:
+            raise ValueError("Width and Height cannot be negative")
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
