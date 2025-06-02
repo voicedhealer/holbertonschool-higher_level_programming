9357dc985046c5464200bb264551925039b65d42
voicedhealer <vivien.bernardot@gmail.com>
2025-06-02 13:41:22 +0200

    exercice 1 write file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
 """
-fonction read file
+Ce module contient une fonction pour écrire une
+chaîne de caractères dans un fichier.
 """
 
 
 def write_file(filename="", text=""):
     """
-    definition de filename en écriture
-    """
+    Écrit une chaîne de caractères dans un fichier texte (UTF-8)
+    et retourne le nombre de caractères écrits.
 
-    with open(filename, 'w', encoding="utf8") as f:
-        contenu = f.write
-        print(contenu, end="")
+    Args:
+        filename (str): Le nom du fichier dans lequel écrire.
+                        Le fichier sera créé s'il n'existe pas,
+                        et son contenu sera écrasé s'il existe.
+        text (str): La chaîne de caractères à écrire dans le fichier.
+
+    Returns:
+        int: Le nombre de caractères écrits.
+    """
+    with open(filename, mode='w', encoding="utf-8") as f:
+        nb_characters_written = f.write(text)
+        return nb_characters_written
