81309ba1d60191601a36f4e9e30cb0ec9c201e91
voicedhealer <vivien.bernardot@gmail.com>
2025-06-02 11:59:37 +0200

    inital commit and task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,6 +8,6 @@ def read_file(filename=""):
     """
     definition de le fonction read file
     """
-    with open('filename', 'r', encoding="utf8") as f:
+    with open(filename, 'r', encoding="utf8") as f:
         contenu = f.read()
         print(contenu, end="")
