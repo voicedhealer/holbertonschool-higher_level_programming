4d231494d23b59f4232f3b7d165b4f42edda7bc9
voicedhealer <vivien.bernardot@gmail.com>
2025-06-06 15:11:59 +0200

    exercice 7 add item

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+import os
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+
+filename = "add_item.json"
+
+
+if os.path.exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
+
+items.extend(sys.argv[1:])
+
+save_to_json_file(items, filename)
