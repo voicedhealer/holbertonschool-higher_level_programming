35f922caec05cd863d09b00e39758b5d060298ee
voicedhealer <vivien.bernardot@gmail.com>
2025-06-06 15:16:50 +0200

    exercice 7 add item

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+"""
+Script qui ajoute tous les arguments de la ligne de commande à une liste Python
+et les sauvegarde dans un fichier JSON ('add_item.json').
+
+- Utilise save_to_json_file pour la sérialisation.
+- Utilise load_from_json_file pour la désérialisation.
+- Crée le fichier s'il n'existe pas.
+"""
+
 import sys
 import os
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
-
 
 if os.path.exists(filename):
     items = load_from_json_file(filename)
