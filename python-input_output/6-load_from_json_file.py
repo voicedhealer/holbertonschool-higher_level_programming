0e72aa531fedbaf31114f7aa18a25a308708b321
voicedhealer <vivien.bernardot@gmail.com>
2025-06-06 12:23:30 +0200

    exercice 6 create object a json

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Crée un objet Python à partir d’un fichier contenant du JSON.
+
+    Args:
+        filename (str): Le chemin du fichier JSON à lire.
+
+    Returns:
+        L’objet Python désérialisé depuis le fichier.
+    """
+    with open(filename, 'r') as f:
+        return json.load(f)
