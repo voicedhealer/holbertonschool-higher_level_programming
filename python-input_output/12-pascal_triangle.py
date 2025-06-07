e1ba8ac2c1bac8c675d7737d33616f16bf629a04
voicedhealer <vivien.bernardot@gmail.com>
2025-06-07 11:09:37 +0200

    exercice 12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+def pascal_triangle(n):
+    """
+    Retourne une liste de listes d'entiers
+    représentant le triangle de Pascal de n.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        # Chaque ligne commence et finit par 1
+        row = [1]
+        # Calcul des valeurs intermédiaires
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
