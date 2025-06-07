eb1c82a829e58951421456e878ecefed8cb0dc72
voicedhealer <vivien.bernardot@gmail.com>
2025-06-07 11:00:50 +0200

    exercice 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+module etudiant
+"""
+
+
+class Student:
+    """
+classe étudiant
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        initialisation de irst name , last name et age
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        conversion en format json
+        """
+        if (
+                isinstance(attrs, list)
+                and all(isinstance(item, str) for item in attrs)
+        ):
+            return {
+                k: self.__dict__[k]
+                for k in attrs if k in self.__dict__
+                    }
+        return self.__dict__
