d5c0867943f8439dd64748554d4dc3af67eb258d
vivien bernardot <42698184+voicedhealer@users.noreply.github.com>
2025-06-09 12:13:55 +0200

    essaie

--- restful-api/test.js
+++ restful-api/test.js
@@ -1,5 +1,13 @@
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-.then(response => response.json())
-.then(json => console.log(json));
-
-console.log("Hello, world!");
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'mcb3d',
+    body: 'object 3d',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then(response => response.json())
+  .then(json => console.log(json));
