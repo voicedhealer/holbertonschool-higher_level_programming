6a9fb1893c1b89ab0e2a6493ee01dc951d593f62
voicedhealer <vivien.bernardot@gmail.com>
2025-06-10 14:47:41 +0200

    exercice 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -33,7 +33,7 @@ class Http(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"Endpoint Not Found")
+            self.wfile.write(b"Endpoint not found")
 
 if __name__ == "__main__":
     server_address = ('', 8000)
