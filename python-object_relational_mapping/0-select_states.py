e804fe1b1c3ce7e7ad385506b16d4d17d0293501
voicedhealer <vivien.bernardot@gmail.com>
2025-06-25 09:50:02 +0200

    exercice 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,12 +7,11 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Récupère les arguments de la ligne de commande.
+
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-    # Établir la connexion à la base de données MySQL.
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -21,20 +20,14 @@ if __name__ == "__main__":
         db=database_name
     )
 
-    # Créer un objet curseur pour exécuter des requêtes SQL.
     cursor = db.cursor()
 
-    # Exécuter la requête SQL pour sélectionner tous les états, triés par ID.
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Récupérer toutes les lignes du résultat de la requête.
     rows = cursor.fetchall()
 
-    # Afficher chaque état. La fonction print() sur un tuple
-    # produit le format attendu (id, 'nom').
     for row in rows:
         print(row)
 
-    # Fermer le curseur et la connexion.
     cursor.close()
     db.close()
