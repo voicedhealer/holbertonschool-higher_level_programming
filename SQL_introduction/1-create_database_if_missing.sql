dd5e1e5948d380baec416fdfe2b8bdab6dbbf9b4
voicedhealer <vivien.bernardot@gmail.com>
2025-06-25 15:42:31 +0200

    exercice 1

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- This SQL command creates a new database named 'hbtn_0c_0' if it does not already exist.
+-- If the database already exists, the command will not raise an error.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
