928876ee6346acec5b1d4be2466da2e6f29e55bc
voicedhealer <vivien.bernardot@gmail.com>
2025-06-27 12:02:58 +0200

    exercice 7 state

---
+++ python-object_relational_mapping/7-model_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Définition du modèle State pour SQLAlchemy ORM
+"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+# Création de la classe de base pour tous les modèles
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Modèle représentant un état dans la base de données
+
+    Attributes:
+        id (int): Identifiant unique de l'état (clé primaire)
+        name (str): Nom de l'état (jusqu'à 128 caractères)
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
