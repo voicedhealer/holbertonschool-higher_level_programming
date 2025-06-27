47272bf062539f7be7f011203fc0f2c565cca1ab
voicedhealer <vivien.bernardot@gmail.com>
2025-06-27 10:29:34 +0200

    update exercice 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-"""
-Script qui liste tous les États de
-la base de données hbtn_0e_0_usa.
-"""
-
+'''
+Module to list all states from a specified db
+'''
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    name = sys.argv[3]
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    db_name = sys.argv[3]
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=name
-    )
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=user, passwd=pwd, db=db_name)
 
     cursor = db.cursor()
+
     cursor.execute("SELECT id, name FROM states ORDER BY id ASC;")
     rows = cursor.fetchall()
 
