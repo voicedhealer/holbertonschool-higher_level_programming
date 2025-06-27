58574554725aff02ed22992f6b6d924b0542045d
voicedhealer <vivien.bernardot@gmail.com>
2025-06-27 12:05:00 +0200

    update exercice 7 state

--- python-object_relational_mapping/7-model_state.py
+++ python-object_relational_mapping/7-model_state.py
@@ -19,5 +19,5 @@ class State(Base):
     """
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
