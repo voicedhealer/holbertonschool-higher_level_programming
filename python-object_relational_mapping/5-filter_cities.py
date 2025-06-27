c5d14ecebcb020f2e6d74ca5bc2cb752498d09a1
voicedhealer <vivien.bernardot@gmail.com>
2025-06-27 11:50:40 +0200

    exercice 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,55 +1,26 @@
 #!/usr/bin/python3
-"""
-Script qui se connecte à une base de données MySQL et affiche
-toutes les villes d'un état spécifique.
-"""
+'''Lists all cities of a given state'''
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Récupérer les arguments en ligne de commande
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    db_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    try:
-        # Établir une connexion à la base de données MySQL
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=mysql_username,
-            passwd=mysql_password,
-            db=database_name
-        )
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=user, passwd=pwd, db=db_name)
 
-        # Créer un objet curseur pour exécuter des requêtes SQL
-        cursor = db.cursor()
+    cursor = db.cursor()
+    query = ("SELECT cities.name FROM cities "
+             "JOIN states ON cities.state_id = states.id "
+             "WHERE states.name = %s ORDER BY cities.id ASC;")
+    cursor.execute(query, (state_name,))
+    rows = cursor.fetchall()
 
-        # Requête SQL sécurisée avec jointure et filtrage
-        query = """
-        SELECT cities.name
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
-        ORDER BY cities.id ASC
-        """
+    print(", ".join([row[0] for row in rows]))
 
-        cursor.execute(query, (state_name,))
-
-        # Récupérer tous les résultats de la requête
-        results = cursor.fetchall()
-
-        # Afficher les résultats dans le format requis
-        city_names = [row[0] for row in results]
-        if city_names:
-            print(", ".join(city_names))
-
-    except MySQLdb.Error as e:
-        print(f"Erreur de connexion ou de requête : {e}")
-    finally:
-        # Fermer le curseur et la connexion à la base de données
-        if 'cursor' in locals() and cursor:
-            cursor.close()
-        if 'db' in locals() and db:
-            db.close()
+    cursor.close()
+    db.close()
