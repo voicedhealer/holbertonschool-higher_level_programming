2932341b7101cf027765587d22cd7e9acd455d9c
voicedhealer <vivien.bernardot@gmail.com>
2025-06-27 09:48:18 +0200

    exercice 12

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- Met à jour le score de "Bob" à 10 dans la table second_table.
+-- Utilise le champ 'name' pour identifier l'enregistrement.
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = 'Bob';
