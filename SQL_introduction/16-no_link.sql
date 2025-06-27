ac00e593aa3da9c55020ac929edaf5cb94b40f11
voicedhealer <vivien.bernardot@gmail.com>
2025-06-27 10:03:20 +0200

    exercice 16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,5 @@
+-- Ce script liste les enregistrements de la table second_table
+-- en excluant ceux dont le nom est NULL ou vide, triés par score.
 SELECT score, name
 FROM second_table
 WHERE name IS NOT NULL AND name != ''
