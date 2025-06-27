c496623c903b3ca76a261b1ad7ad13bc8a7314b1
voicedhealer <vivien.bernardot@gmail.com>
2025-06-27 10:45:32 +0200

    update exercice 0

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,45 +1,22 @@
 #!/usr/bin/python3
 """
-Script qui se connecte à une base de données MySQL et récupère
-toutes les entrées de la table 'states'.
+Script that defines the City class with a relationship to State.
 """
-import MySQLdb
-import sys
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
-if __name__ == "__main__":
-    # Récupérer les arguments en ligne de commande
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
 
-    try:
-        # Établir une connexion à la base de données MySQL
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=mysql_username,
-            passwd=mysql_password,
-            db=database_name
-        )
+Base = declarative_base()
 
-        # Créer un objet curseur pour exécuter des requêtes SQL
-        cursor = db.cursor()
 
-        # Exécuter la requête SQL pour récupérer tous les états, triés par id
-        cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
-
-        # Récupérer tous les résultats de la requête
-        results = cursor.fetchall()
-
-        # Afficher les résultats
-        for row in results:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print(f"Erreur de connexion ou de requête : {e}")
-    finally:
-        # Fermer le curseur et la connexion à la base de données
-        if 'cursor' in locals() and cursor:
-            cursor.close()
-        if 'db' in locals() and db:
-            db.close()
+class City(Base):
+    """
+    City class linked to the MySQL table cities.
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship("State", back_populates="cities")
+    
