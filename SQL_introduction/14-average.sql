7079487928701fcb9879a1e62d17905d0da7df07
voicedhealer <vivien.bernardot@gmail.com>
2025-06-27 09:53:44 +0200

    exercice 14

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- Calcule la moyenne des scores de tous les enregistrements
+-- dans la table second_table.
+-- La colonne de résultat est nommée 'average'.
+SELECT AVG(score) AS average
+FROM second_table;
