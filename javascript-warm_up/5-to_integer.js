778c86c27f4a8cf73c06cbfb9fe69821a67c6740
voicedhealer <vivien.bernardot@gmail.com>
2025-07-12 17:06:25 +0200

    exercice 5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const arg = process.argv[2];
+const number = parseInt(arg);
+
+if (isNaN(number)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + number);
+}
