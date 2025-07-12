ba6e521ec2b5627341652bcb0d12a2bc9bf194b8
voicedhealer <vivien.bernardot@gmail.com>
2025-07-12 17:33:06 +0200

    exercice 11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const args = process.argv.slice(2).map(Number);
+
+if (args.length < 2) {
+  console.log(0);
+} else {
+  // Supprimer les doublons
+  const uniqueArgs = [...new Set(args)];
+  if (uniqueArgs.length < 2) {
+    console.log(0);
+  } else {
+    uniqueArgs.sort((a, b) => b - a); // Tri décroissant
+    console.log(uniqueArgs[1]);
+  }
+}
