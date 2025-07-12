14e397055acb0fad4b2ac1004b75e5ee72688430
voicedhealer <vivien.bernardot@gmail.com>
2025-07-12 16:56:59 +0200

    exercice 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-if (args.length === 0) {
-  console.log('No arguments');
-} else if (args.length === 1) {
+const argsCount = process.argv.length - 2;
+
+if (argsCount === 0) {
+  console.log('No argument');
+} else if (argsCount === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
