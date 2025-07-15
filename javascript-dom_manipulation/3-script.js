e9814df68b961326cf030221f4705dc26e34e909
voicedhealer <vivien.bernardot@gmail.com>
2025-07-15 11:05:49 +0200

    exercice 1

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+document.getElementById('toggle_header').addEventListener('click', function () {
+  const header = document.querySelector('header');
+  if (header.classList.contains('red')) {
+    header.classList.remove('red');
+    header.classList.add('green');
+  } else {
+    header.classList.remove('green');
+    header.classList.add('red');
+  }
+});
